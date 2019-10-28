047d7aa2705f360890f0624666f53a8943801663
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-28 14:37:14 +0100

    scaffolding

---
+++ paridispari/script/main.js
@@ -0,0 +1,82 @@
+// var parola1 = prompt ("inserisci parola1");
+
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// var parola1 = prompt ("inserisci parola1");
+// var parola2 = prompt ("inserisci parola2");
+//
+// // console.log(parola1.length);
+// // console.log(parola2.length);
+//
+// if (parola1.length < parola2.length) {
+//
+//   console.log(parola1);
+//   console.log(parola2);
+//
+// } else if (parola2.length < parola1.length) {
+//
+//   console.log(parola2);
+//   console.log(parola1);
+//
+// } else {
+//   console.log(parola1 , parola2);
+// }
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+// var num1 = parseInt(prompt ("inserisci num1"));
+// var num2 = parseInt(prompt ("inserisci num2"));
+// var num3 = parseInt(prompt ("inserisci num3"));
+// var num4 = parseInt(prompt ("inserisci num4"));
+// var num5 = parseInt(prompt ("inserisci num5"));
+//
+// console.log(num1 + num2 + num3 + num4 + num5);
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+var invitati = ["luca", "marco", "mario"];
+
+var askname = prompt("Come ti chiami");
+
+var inlista = false;
+// ciclo
+
+// usa ciclo while:
+
+
+
+
+// for (var index = 0; index < invitati.length; index++) {
+//   if(askname == invitati[index]) {
+//     inlista=true;
+//     console.log("Sei in lista!");
+//   } else if (inlista = false) {
+//     console.log("Non sei in lista!");
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// IDEA:
