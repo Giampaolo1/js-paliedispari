3bd9e743790974603378e1860f366fbab37aba1e
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-28 16:31:10 +0100

    a

--- palindroma/script/main.js
+++ palindroma/script/main.js
@@ -1,2 +1,11 @@
 // Crea una funzione per capire se la parola è palindroma
 // IDEA: stringa revert
+
+// IDEA: chiedo di inserire una parola
+
+var parola = parola;
+
+// IDEA: creo la funzione revertare la parola
+function reverse (parola) {
+ console.log(parola);
+}
