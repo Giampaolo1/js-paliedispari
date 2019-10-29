bd6cddc2d9b9a372e38dc6380fabfdc1bd104a31
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-29 17:38:07 +0100

    par disp

--- palindroma/script/main.js
+++ palindroma/script/main.js
@@ -2,29 +2,101 @@
 
 // chiedo di inserire una parola
 
-var parola = "rossi";
+var parola = prompt("Inserisci una parola");
 
-// inserisco la parola in un array
-var array = [];
+// eseguo funzione
 
-for (var i = 0; i < parola.length; i++) {
-  array.push (parola[i]);
+palindroma (parola);
+// palindroma ("stringa");
+// palindromabianchi ();
+// palindromabianchi ();
+
+
+// funzione:
+
+function palindroma (parolainput) {
+  // inserisco la parola in un array
+  // var array = [];
+  //
+  // for (var i = 0; i < parolainput.length; i++) {
+  //   array.push (parolainput[i]);
+  // }
+  // console.log(array);
+
+
+  // inverto la parola e la metto in un altro array
+
+  // var arrayVuoto = [];
+
+  var revertata = "";
+
+  for (var i = parolainput.length -1; i >= 0 ; i--) {
+
+    revertata = revertata + parolainput[i];
+
+
+
+   // arrayVuoto.push (parolainput[i]);
+
+
+
+
+  // if (arrayVuoto==array) {
+  //   console.log("Palindroma");
+  // } else {
+  //   console.log("Non Palindroma");
+  // }
+
+  }
+
+  console.log("la parola revertata è: " + revertata);
+  // console.log(arrayVuoto);
+
+  // if (array == arrayVuoto) {
+  //   console.log("Palindroma!");
+  // } else {
+  //   console.log("non palindroma");
+  // }
+
+  if (parola == revertata) {
+    console.log("Palindroma");
+  } else {
+    console.log("Non Palindroma");
+  }
 }
-console.log(array);
 
 
-// inverto la parola e la metto in un altro array
 
-var arrayVuoto = [];
 
-for (var i = parola.length; i >= 0 ; i--) {
- arrayVuoto.push (parola[i]);
 
-// if (arrayVuoto==array) {
-//   console.log("Palindroma");
-// } else {
-//   console.log("Non Palindroma");
-// }
+
+
+
+// funzione2:
+
+function palindromabianchi () {
+  var stringafissa = "bianchi";
+  // inserisco la parola in un array
+  var array = [];
+
+  for (var i = 0; i < stringafissa.length; i++) {
+    array.push (stringafissa[i]);
+  }
+  console.log(array);
+
+
+  // inverto la parola e la metto in un altro array
+
+  var arrayVuoto = [];
+
+  for (var i = stringafissa.length -1; i >= 0 ; i--) {
+   arrayVuoto.push (stringafissa[i]);
+
+  // if (arrayVuoto==array) {
+  //   console.log("Palindroma");
+  // } else {
+  //   console.log("Non Palindroma");
+  // }
+  }
+  console.log(arrayVuoto);
 }
-
-console.log(arrayVuoto);
--- paridispari/script/main.js
+++ paridispari/script/main.js
@@ -1,6 +1,48 @@
-// L’utente sceglie pari o dispari e un numero
-// da 1 a 5.
+// L’utente sceglie pari o dispari
+// e un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1
 // a 5) per il computer.
 // Sommiamo i due numeri e dichiariamo chi ha
 // vinto.
+
+// L’utente sceglie pari o dispari
+
+var padispUtente = prompt ("pari o dispari")
+console.log(padispUtente);
+
+// e un numero da 1 a 5.
+var numUtente = parseInt(prompt("da 1 a 5"))
+console.log(numUtente);
+
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+
+// invoca la funzione e dagli min 1 e max 5
+var numPC = numRandom (1,5)
+
+// invoca la fx pariDisp
+var vittoria = pariDisp (numUtente, numPC)
+
+console.log(numPC);
+
+// crea la funzione per il numero numRandom
+
+function numRandom (min, max) {
+  return Math.floor(Math.random()*(max-min+1)) + min;
+}
+
+// crea la funzione per pari e dispari
+
+function pariDisp (num1,num2){
+  var somma = num1 + num2;
+  if (somma % 2 == 0) {
+    return "pari";
+  } else {
+    return "dispari"
+  }
+}
+
+// output finale
+
+
+console.log("Vince il " + vittoria);
